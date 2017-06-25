210984081801d553bc414c115119d54aacee4646
Ha Minh Nghia <nghia@naxam.net>
2017-06-25 11:34:35 +0700

    Complete binding side menu

---
+++ SideMenuQs/Code/Fragment/ContentFragment.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.Support.V7.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Yalantis.Com.Sidemenu.Interfaces;
+using Yalantis.Com.Sidemenu.Util;
+using static Android.Resource;
+using Android.Support.V4.App;
+using Java.Lang;
+
+namespace SideMenuQs.Code
+{
+    class ContentFragment : Fragment, IScreenShotable
+    {
+
+        public const string CLOSE = "Close";
+        public const string BUILDING = "Building";
+        public const string BOOK = "Book";
+        public const string PAINT = "Paint";
+        public const string CASE = "Case";
+        public const string SHOP = "Shop";
+        public const string PARTY = "Party";
+        public const string MOVIE = "Movie";
+
+        private View containerView;
+        protected ImageView mImageView;
+        protected int res;
+        private Bitmap bitmap;
+
+
+        public static ContentFragment NewInstance(int resId)
+        {
+            ContentFragment contentFragment = new ContentFragment();
+            Bundle bundle = new Bundle();
+            bundle.PutInt(nameof(Android.Resource.Integer), resId);
+            contentFragment.Arguments = bundle;
+            return contentFragment;
+        }
+
+        public override void OnViewCreated(View view, Bundle savedInstanceState)
+        {
+            base.OnViewCreated(view, savedInstanceState);
+            this.containerView = view.FindViewById(Resource.Id.container);
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            View rootView = inflater.Inflate(Resource.Layout.fragment_main, container, false);
+            mImageView = rootView.FindViewById<ImageView>(Resource.Id.image_content);
+            mImageView.Clickable = true;
+            mImageView.Focusable = true;
+            mImageView.SetImageResource(res);
+
+            return rootView;
+        }
+
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            res = Arguments.GetInt(nameof(Android.Resource.Integer));
+        }
+
+
+        public Bitmap Bitmap
+        {
+            get { return bitmap; }
+        }
+
+        public void TakeScreenShot()
+        {
+            Thread thread = new Thread(() =>
+            {
+                Bitmap bitmap = Bitmap.CreateBitmap(containerView.Width, containerView.Height, Bitmap.Config.Argb8888);
+                Canvas canvas = new Canvas(bitmap);
+                containerView.Draw(canvas);
+                this.bitmap = bitmap;
+            });
+        }
+    }
+}
---
+++ SideMenuQs/Code/MainActivity.cs
@@ -0,0 +1,229 @@
+﻿using Android.App;
+using Android.OS;
+using Yalantis.Com.Sidemenu.Interfaces;
+using Yalantis.Com.Sidemenu.Animation;
+using Yalantis.Com.Sidemenu.Model;
+using SideMenuQs.Code;
+using System.Collections.Generic;
+using Android.Support.V4.Widget;
+using Android.Graphics;
+using Android.Support.V7.AppCompat;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+using Android.Widget;
+using Yalantis.Com.Sidemenu.Util;
+using IO.Codetail.Animation;
+using Android.Views;
+using System;
+using Android.Views.Animations;
+using Android.Graphics.Drawables;
+using Android.Content.Res;
+
+namespace SideMenuQs
+{
+    [Activity(Label = "SideMenuQs", MainLauncher = true, Icon = "@drawable/icon",Theme = "@style/AppTheme")]
+    public class MainActivity : ActionBarActivity, Yalantis.Com.Sidemenu.Util.ViewAnimator.IViewAnimatorListener
+    {
+        private List<SlideMenuItem> listMenuItems = new List<SlideMenuItem>();
+        private Yalantis.Com.Sidemenu.Util.ViewAnimator viewAnimator;
+        private DrawerLayout drawerLayout;
+        private ActionBarDrawerToggle drawerToggle;
+        private ContentFragment contentFragment;
+        private int res = Resource.Drawable.content_music;
+        private LinearLayout linearLayout;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            contentFragment = ContentFragment.NewInstance(Resource.Drawable.content_music);
+            SupportFragmentManager.BeginTransaction()
+                .Replace(Resource.Id.content_frame, contentFragment)
+                .Commit();
+
+            drawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            drawerLayout.SetScrimColor(Color.Transparent);
+            linearLayout = FindViewById<LinearLayout>(Resource.Id.left_drawer);
+            linearLayout.Click += (s, e) =>
+            {
+                drawerLayout.CloseDrawers();
+            };
+
+            SetActionBar();
+            CreateMenuList();
+
+            viewAnimator = new Yalantis.Com.Sidemenu.Util.ViewAnimator(this, listMenuItems, contentFragment, drawerLayout, this);
+
+        }
+
+        public void CreateMenuList()
+        {
+            SlideMenuItem menuItem0 = new SlideMenuItem("CLOSE", Resource.Drawable.icn_close);
+            listMenuItems.Add(menuItem0);
+            SlideMenuItem menuItem = new SlideMenuItem("BUILDING", Resource.Drawable.icn_1);
+            listMenuItems.Add(menuItem);
+            SlideMenuItem menuItem2 = new SlideMenuItem("BOOK", Resource.Drawable.icn_2);
+            listMenuItems.Add(menuItem2);
+            SlideMenuItem menuItem3 = new SlideMenuItem("PAINT", Resource.Drawable.icn_3);
+            listMenuItems.Add(menuItem3);
+            SlideMenuItem menuItem4 = new SlideMenuItem("CASE", Resource.Drawable.icn_4);
+            listMenuItems.Add(menuItem4);
+            SlideMenuItem menuItem5 = new SlideMenuItem("SHOP", Resource.Drawable.icn_5);
+            listMenuItems.Add(menuItem5);
+            SlideMenuItem menuItem6 = new SlideMenuItem("PARTY", Resource.Drawable.icn_6);
+            listMenuItems.Add(menuItem6);
+            SlideMenuItem menuItem7 = new SlideMenuItem("MOVIE", Resource.Drawable.icn_7);
+            listMenuItems.Add(menuItem7);
+        }
+
+        public void SetActionBar()
+        {
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+            SupportActionBar.SetHomeButtonEnabled(true);
+            drawerToggle = new ActionBarDrawerToggle0(
+                this,
+                drawerLayout,
+                toolbar,
+                Resource.String.drawer_open,
+                Resource.String.drawer_close)
+            {
+                DrawerClosed = (View view) =>
+                {
+                    linearLayout.RemoveAllViews();
+                    linearLayout.Invalidate();
+                },
+                DrawerSlide = (View drawerView, float slideOffset) =>
+                {
+                    if (slideOffset > 0.6 && linearLayout.ChildCount == 0) viewAnimator.ShowMenuContent();
+                },
+                DrawerOpened = (View drawerView) =>
+                {
+
+                }
+            };
+
+            drawerLayout.SetDrawerListener(drawerToggle);
+        }
+
+        public override void OnPostCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnPostCreate(savedInstanceState, persistentState);
+            drawerToggle.SyncState();
+        }
+
+        public override void OnConfigurationChanged(Configuration newConfig)
+        {
+            base.OnConfigurationChanged(newConfig);
+            drawerToggle.OnConfigurationChanged(newConfig);
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            if (drawerToggle.OnOptionsItemSelected(item))
+            {
+                return true;
+            }
+
+            switch (item.ItemId)
+            {
+                case Resource.Id.action_settings:
+                    return true;
+                default: return base.OnOptionsItemSelected(item);
+            }
+        }
+
+        public IScreenShotable ReplaceFragment(IScreenShotable screenShotable, int topPosition)
+        {
+            this.res = this.res == Resource.Drawable.content_music ? Resource.Drawable.content_films : Resource.Drawable.content_music;
+            View view = FindViewById(Resource.Id.content_frame);
+            int finalRadius = Math.Max(view.Width, view.Height);
+            SupportAnimator animator = IO.Codetail.Animation.ViewAnimationUtils.CreateCircularReveal(view, 0, topPosition, 0, finalRadius);
+            animator.SetInterpolator(new AccelerateInterpolator());
+            animator.SetDuration(Yalantis.Com.Sidemenu.Util.ViewAnimator.CircularRevealAnimationDuration);
+
+            FindViewById(Resource.Id.content_overlay).SetBackgroundDrawable(new BitmapDrawable(Resources, screenShotable.Bitmap));
+            animator.Start();
+            ContentFragment contentFragment = ContentFragment.NewInstance(this.res);
+            SupportFragmentManager.BeginTransaction().Replace(Resource.Id.content_frame, contentFragment).Commit();
+            return contentFragment;
+        }
+
+        public void AddViewToContainer(View p0)
+        {
+            linearLayout.AddView(p0);
+        }
+
+        public void DisableHomeButton()
+        {
+            SupportActionBar.SetHomeButtonEnabled(false);
+        }
+
+        public void EnableHomeButton()
+        {
+            SupportActionBar.SetHomeButtonEnabled(true);
+            drawerLayout.CloseDrawers();
+        }
+
+        public IScreenShotable OnSwitch(IResourceble p0, IScreenShotable p1, int p2)
+        {
+            switch (p0.Name)
+            {
+                case ContentFragment.CLOSE:
+                    return p1;
+                default:
+                    return ReplaceFragment(p1, p2);
+            }
+        }
+
+    }
+    public class ActionBarDrawerToggle0 : ActionBarDrawerToggle
+    {
+        public Action<View> DrawerClosed { set; get; }
+        public Action<View, float> DrawerSlide { get; set; }
+        public Action<View> DrawerOpened { get; set; }
+
+        public ActionBarDrawerToggle0(Activity activity,
+            DrawerLayout drawerLayout,
+            Android.Support.V7.Widget.Toolbar toolbar,
+            int openDrawerContentDescRes,
+            int closeDrawerContentDescRes)
+            : base(activity,
+                drawerLayout,
+                toolbar,
+                openDrawerContentDescRes,
+                closeDrawerContentDescRes)
+        {
+
+        }
+
+        public override void OnDrawerClosed(View drawerView)
+        {
+            base.OnDrawerClosed(drawerView);
+            DrawerClosed?.Invoke(drawerView);
+        }
+
+        public override void OnDrawerSlide(View drawerView, float slideOffset)
+        {
+            base.OnDrawerSlide(drawerView, slideOffset);
+            DrawerSlide?.Invoke(drawerView, slideOffset);
+        }
+
+        public override void OnDrawerOpened(View drawerView)
+        {
+            base.OnDrawerOpened(drawerView);
+            DrawerOpened?.Invoke(drawerView);
+        }
+    }
+
+}
+
